6fcfb8033016ec10347d1dd847ad4fc08097faa5
Lilesh-Patil-Dev <lileshpatil38@gmail.com>
2026-05-04 22:00:02 +0530

    Day 7

--- Learn/src/main/java/com/SpringSeries/Learn/entity/LearnEntity.java
+++ Learn/src/main/java/com/SpringSeries/Learn/entity/LearnEntity.java
@@ -1,29 +1,14 @@
 package com.SpringSeries.Learn.entity;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "Entity")
+@Data
 public class LearnEntity {
-
     @Id
     private ObjectId id;
     private String name;
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
